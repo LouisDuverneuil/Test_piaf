a6f8441c9a8c09f1fbcdacd0a81ca52c471d1458
Duverneuil Louis <44844278+LouisDuverneuil@users.noreply.github.com>
2021-01-18 11:15:10 +0100

    Update run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -6,4 +6,4 @@ if [[ ! -d "src/staticfiles" ]]; then python src/manage.py collectstatic --noinp
 
 python src/manage.py wait_for_db
 python src/manage.py migrate
-gunicorn --bind="0.0.0.0:${PORT:-8040}" --workers="${WORKERS:-1}" --pythonpath=app app.wsgi --timeout 300
+gunicorn --bind="0.0.0.0:${PORT:-8000}" --workers="${WORKERS:-1}" --pythonpath=app app.wsgi --timeout 300
